4cc036b039a0910d506b054a19f95e50f551587c
achyuatkumar <90673749+achyuatkumar@users.noreply.github.com>
2022-07-14 15:25:11 +0530

    Update connection_string.py

--- connection_string.py
+++ connection_string.py
@@ -8,3 +8,23 @@ username = 'myusername'
 password = 'mypassword' 
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
+@app.route("/login")
+def login():
+
+  username = request.values.get('username')
+  password = request.values.get('password')
+
+  # Prepare database connection
+  db = pymysql.connect("localhost")
+  cursor = db.cursor()
+
+  # Execute the vulnerable SQL query concatenating user-provided input.
+  cursor.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password))
+
+  # If the query returns any matching record, consider the current user logged in.
+  record = cursor.fetchone()
+  if record:
+    session['logged_user'] = username
+
+  # disconnect from server
+  db.close()
